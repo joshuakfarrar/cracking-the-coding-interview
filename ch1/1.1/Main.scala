e0a39533857d1c0da924cc2283d51720df579ef4
Joshua K. Farrar <joshua.farrar@gmail.com>
2022-02-16 15:26:03 -0500

    exercise 1.1, post whiteboard

---
+++ ch1/1.1/Main.scala
@@ -0,0 +1,42 @@
+import cats.effect._
+
+object TestSuite {
+  import Application.unique
+
+  val testA = IO {
+    unique("abc") == true
+  }
+
+  val testB = IO {
+    unique("aa") == false
+  }
+
+  val tests = for {
+    a <- testA
+    b <- testB
+  } yield a && b
+}
+
+object Application extends IOApp {
+  def unique(a: String): Boolean = {
+    def internal(b: String)(seen: String): Boolean = {
+      b.headOption match {
+        case Some(c) =>
+          if (seen.contains(c)) false
+          else internal(b.tail)(seen :+ c)
+        case None => true
+      }
+    }
+    internal(a)("")
+  }
+
+  def resultToYesNo(a: Boolean): String = a match {
+    case true => "yes"
+    case false => "no"
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = for {
+    result <- TestSuite.tests
+    _ <- IO(println(s"did all tests pass?: ${resultToYesNo(result)}"))
+  } yield ExitCode.Success
+}
